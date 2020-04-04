be089da53b4a8900bd75939a3096d1bd77ceb84a
radit26 <radit2639@gmail.com>
2020-04-04 17:58:11 +0100

    Created default views and declared application

--- itservices/reviewApp/views.py
+++ itservices/reviewApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+# from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return render(request, 'reviewApp/home.html')
+
+def about(request):
+    return render(request, 'reviewApp/about.html')
+
+def contact(request):
+    return render(request, 'reviewApp/contact.html')
+
+def products(request):
+    return render(request, 'reviewApp/products.html')
