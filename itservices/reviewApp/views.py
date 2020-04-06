0e7071482611d2f5a448c056405af4c49e016e15
radit26 <radit2639@gmail.com>
2020-04-06 19:07:55 +0100

    css added

--- itservices/reviewApp/views.py
+++ itservices/reviewApp/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'reviewApp/home.html')
+    return render(request, 'reviewApp/home.html', {'title': 'Home'})
 
 def about(request):
-    return render(request, 'reviewApp/about.html')
+    return render(request, 'reviewApp/about.html', {'title': 'About Us'})
 
 def contact(request):
-    return render(request, 'reviewApp/contact.html')
+    return render(request, 'reviewApp/contact.html', {'title': 'Contact Us'})
 
 def products(request):
     return render(request, 'reviewApp/products.html')
