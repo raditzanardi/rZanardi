2f93f21550d96e1d49e3c682d8a8164638b9566b
radit26 <radit2639@gmail.com>
2020-04-08 15:18:45 +0100

    Added constraints to Profile model and created Review model

--- itservices/reviewApp/models.py
+++ itservices/reviewApp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from users.models import Profile
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Product(models.Model):
 	name = models.CharField(max_length=100)
@@ -17,4 +19,16 @@ class Product(models.Model):
 	def get_absolute_url(self):
 		return reverse('product-detail', kwargs={'pk':self.pk})
 
+class Review(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	rating = models.PositiveSmallIntegerField(default=1, validators = [MinValueValidator(1), MaxValueValidator(5)])
+	reviewtext = models.TextField()
+	postdate = models.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return f'Review for {self.product} by {self.author} on {self.postdate}'
+
+	def get_absolute_url(self):
+		return reverse('review-detail', kwargs={'pk':self.pk})
